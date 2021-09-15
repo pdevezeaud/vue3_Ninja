87b0585505f3cb2abf05324a702e5371b8b3331c
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2021-09-15 18:50:44 +0200

    session 2

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data(){
+        return {
+          title: 'The Finale Empire'  
+        }
+    }
+})
+
+app.mount('#app')
+
+
+//11:37 https://www.youtube.com/watch?v=F7PLPJqVotk&t=302s  
