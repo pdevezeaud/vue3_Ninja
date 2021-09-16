d879ae47debf359afd12fc87740d9d1a9cafe034
devezeaud philippe <pdevezeaud@yahoo.fr>
2021-09-16 16:38:29 +0200

    event

--- app.js
+++ app.js
@@ -56,4 +56,4 @@ app3.mount('#app3')
 
 
 
-//22:24 https://www.youtube.com/watch?v=F7PLPJqVotk&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3
+//1:51  https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3
