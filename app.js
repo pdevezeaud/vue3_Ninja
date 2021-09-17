841577458f3bc02374f1f818b02da52f9cff750a
devezeaud philippe <pdevezeaud@yahoo.fr>
2021-09-17 15:59:27 +0200

    v-bin v-for

--- app.js
+++ app.js
@@ -33,27 +33,89 @@
 // app2.mount('#app2')
 
 
-const app3 = Vue.createApp({
-  data(){
-      return {
-        showBooks: true,
-        title: 'The Finale Empire',
-        author: 'Brandon Sanderson',
-        age: 45 
-      }
-  },
-  methods: {
-    toggleShowBooks() {
-      this.showBooks = !this.showBooks
-    }
-  },
+// const app3 = Vue.createApp({
+//   data(){
+//       return {
+//         showBooks: true,
+//         title: 'The Finale Empire',
+//         author: 'Brandon Sanderson',
+//         age: 45,
+//         x:0,
+//         y:0
+//       }
+//   },
+//   methods: {
+//     toggleShowBooks() {
+//       this.showBooks = !this.showBooks
+//     },
+//     handleEvent(e, data){
+//       console.log(e, e.type)
+//       if(data){
+//         console.log(data)
+//       }
+//     },
+//     handleMousemove(e){
+//       this.x = e.offsetX
+//       this.y = e.offsetY
+//     }
+//   }
   
-})
-
-app3.mount('#app3')
+// })
 
 
 
 
 
-//1:51  https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3
+// const app4 = Vue.createApp({
+//     data(){
+//         return {
+//           showBooks: true,
+//           books: [
+//             {title: 'name of the wind', author: 'Patrick Rothfuss'},
+//             {title: 'The way of kings', author: 'Brandon Sanderson'},
+//             {title: 'The final empire', author: 'Brandon Sanderson'}
+//           ] 
+//         }
+//     },
+//     methods: {
+//       toggleShowBooks() {
+//         this.showBooks = !this.showBooks
+//       }
+//     },
+    
+//   })
+  
+//   app4.mount('#app4')
+
+
+
+
+
+
+  const app5 = Vue.createApp({
+    data(){
+        return {
+          url:'http://www.thenetninja.co.uk',
+          showBooks: true,
+          books: [
+            {title: 'name of the wind', author: 'Patrick Rothfuss'},
+            {title: 'The way of kings', author: 'Brandon Sanderson'},
+            {title: 'The final empire', author: 'Brandon Sanderson'}
+          ] 
+        }
+    },
+    methods: {
+      toggleShowBooks() {
+        this.showBooks = !this.showBooks
+      }
+    },
+    
+  })
+  
+  app5.mount('#app5')
+
+
+
+
+
+//18:30  https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=5
