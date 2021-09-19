102953cdeca7d3177c308f8abbc539ebd7c71950
DEVEZEAUD PHILIPPE <pdevezeaud@yahoo.fr>
2021-09-19 12:18:39 +0200

    ajout class css dynamique

--- app.js
+++ app.js
@@ -92,15 +92,41 @@
 
 
 
-  const app5 = Vue.createApp({
+  // const app5 = Vue.createApp({
+  //   data(){
+  //       return {
+  //         url:'http://www.thenetninja.co.uk',
+  //         showBooks: true,
+  //         books: [
+  //           {title: 'name of the wind', author: 'Patrick Rothfuss', img:'assets/1.jpg'},
+  //           {title: 'The way of kings', author: 'Brandon Sanderson', img:'assets/2.jpg'},
+  //           {title: 'The final empire', author: 'Brandon Sanderson', img: 'assets/3.jpg'}
+  //         ] 
+  //       }
+  //   },
+  //   methods: {
+  //     toggleShowBooks() {
+  //       this.showBooks = !this.showBooks
+  //     }
+  //   },
+    
+  // })
+  
+  // app5.mount('#app5')
+
+
+
+
+
+  const app6 = Vue.createApp({
     data(){
         return {
           url:'http://www.thenetninja.co.uk',
           showBooks: true,
           books: [
-            {title: 'name of the wind', author: 'Patrick Rothfuss'},
-            {title: 'The way of kings', author: 'Brandon Sanderson'},
-            {title: 'The final empire', author: 'Brandon Sanderson'}
+            {title: 'name of the wind', author: 'Patrick Rothfuss', img:'assets/1.jpg', isFav:true},
+            {title: 'The way of kings', author: 'Brandon Sanderson', img:'assets/2.jpg', isFav:false},
+            {title: 'The final empire', author: 'Brandon Sanderson', img: 'assets/3.jpg', isFav: true},
           ] 
         }
     },
@@ -112,7 +138,11 @@
     
   })
   
-  app5.mount('#app5')
+  app6.mount('#app6')
+
+
+
+
 
 
 
